1309cd45dbe219f5583cc2cf4b62de5e8e9f0e10
Lucaswhy <Lucas_herculano_2010@hotmail.com>
2021-03-03 22:19:25 -0300

    ++ models e migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models {
+    public class Seller {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
+        public Seller() {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthdate, double baseSalary, Department department) {
+            Id = id;
+            Name = name;
+            Email = email;
+            Birthdate = birthdate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+
+        public void addSales(SalesRecord sr) {
+            Sales.Add(sr);
+        }
+
+        public void removeSales(SalesRecord sr) {
+            Sales.Remove(sr);
+        }
+
+        public double totalSales(DateTime initial, DateTime final) {
+            return Sales.Where(s => s.Date >= initial && s.Date <= final).Sum(s => s.Amount);
+        }
+
+    }
+}
