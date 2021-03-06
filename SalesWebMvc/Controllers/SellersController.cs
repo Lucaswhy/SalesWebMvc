b6f6c6f815d7eeabdda03e2d4102446f2aee4804
Lucaswhy <Lucas_herculano_2010@hotmail.com>
2021-03-05 23:48:23 -0300

    ++ validations

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -37,6 +37,13 @@ namespace SalesWebMvc.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
+
+            if (!ModelState.IsValid) {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -77,6 +84,13 @@ namespace SalesWebMvc.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
+
+            if (!ModelState.IsValid) {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id) return RedirectToAction(nameof(Error), new { message = "Id missmatch" });
             try {
                 _sellerService.Update(seller);
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,8 +6,12 @@ using System.Linq;
 namespace SalesWebMvc.Models {
     public class Seller {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} Required.")]
+        [StringLength(100,MinimumLength = 2,ErrorMessage = "Please, type your name correctly.")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid {0}")]
+        [Required(ErrorMessage = "{0} Required.")]
         public string Email { get; set; }
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
